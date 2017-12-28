87fd8c1bb5f7a32bfdd720408b89bb4dc7fd50ab
takumon <inouetakumon@gmail.com>
2017-12-28 12:29:35 +0900

    モーダルにチャート（仮）を表示するようにした

--- app/scripts/milestones.ts
+++ app/scripts/milestones.ts
@@ -2,6 +2,8 @@ import { Constant } from './constant';
 import { storageUtil } from './storage-util';
 import { milestoneService } from './milestones.service';
 
+declare const Chart: any;
+
 /**
  * マイルストーン画面の初期化処理
  */
@@ -91,7 +93,9 @@ $(() => {
 
   function appendModal() {
     const modal = `
-    <div id="estimation-modal">Estimation modal!</div>
+    <div id="estimation-modal">Estimation modal!
+      <canvas id="estimaitonChartCanvas" height="90%" width="90%"></canvas>
+    </div>
     `;
 
     $('body').append(modal);
@@ -109,6 +113,51 @@ $(() => {
     const top = (windowHight - modalHight) / 2;
 
     $modal.css({left, top}).click(hideModal);
+
+    drawChart($modal.find('#estimaitonChartCanvas') as JQuery<HTMLCanvasElement>);
+  }
+
+  function drawChart($drawArea: JQuery<HTMLCanvasElement>) {
+
+    // jQueryオブジェクト[0]とすれば、getContext("2D")できる。
+    const ctx = $drawArea[0].getContext('2d');
+    // グラフ描画
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+    });
   }
 
   function removeModal() {
